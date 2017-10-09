e2ddf78d953637d5f1fe6185ce2cfb4fe803d401
Nicholas Wolff <nwolff@gmail.com>
2017-10-09 13:15:01 +0200

    ensure we don't forget migrations

--- tests/settings.py
+++ tests/settings.py
@@ -1,13 +1,9 @@
 # flake8: noqa
 
-import os
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 DATABASES = {
     'default': {
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'test.db'
     },
 }
 
