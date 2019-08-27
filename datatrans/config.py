e34dfb38fb01d578a8f86710caf5c9a38cd30bbd
Nicholas Wolff <nwolff@gmail.com>
2019-08-27 12:09:08 +0200

    use new datatrans payment and api urls

--- datatrans/config.py
+++ datatrans/config.py
@@ -1,8 +1,9 @@
-from django.conf import settings
 import hashlib
 import hmac
 import os
 from typing import Any, Sequence
+
+from django.conf import settings
 
 web_merchant_id = settings.DATATRANS['WEB_MERCHANT_ID']
 _web_hmac_key = bytearray.fromhex(settings.DATATRANS['WEB_HMAC_KEY'])
@@ -10,13 +11,15 @@ mpo_merchant_id = settings.DATATRANS['MPO_MERCHANT_ID']
 _mpo_hmac_key = bytearray.fromhex(settings.DATATRANS['MPO_HMAC_KEY'])
 
 if settings.DATATRANS.get('ENVIRONMENT') == 'PRODUCTION':
-    _base_url = 'https://payment.datatrans.biz/'
+    _pay_base_url = 'https://pay.datatrans.com/'
+    _api_base_url = 'https://api.datatrans.com/'
 else:
-    _base_url = 'https://pilot.datatrans.biz/'
+    _pay_base_url = 'https://pay.sandbox.datatrans.com/'
+    _api_base_url = 'https://api.sandbox.datatrans.com/'
 
-datatrans_js_url = os.path.join(_base_url, 'upp/payment/js/datatrans-2.0.0.min.js')
-datatrans_authorize_url = os.path.join(_base_url, 'upp/jsp/XML_authorize.jsp')
-datatrans_processor_url = os.path.join(_base_url, 'upp/jsp/XML_processor.jsp')
+datatrans_js_url = os.path.join(_pay_base_url, 'upp/payment/js/datatrans-2.0.0.min.js')
+datatrans_authorize_url = os.path.join(_api_base_url, 'upp/jsp/XML_authorize.jsp')
+datatrans_processor_url = os.path.join(_api_base_url, 'upp/jsp/XML_processor.jsp')
 
 
 def sign_web(*values: Any) -> str:
