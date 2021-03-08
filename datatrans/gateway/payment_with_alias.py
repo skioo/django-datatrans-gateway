85895ff086f7cc4028c60d02ba35156e1b375c05
Alberto Pou <bertini36@users.noreply.github.com>
2021-03-08 14:16:45 +0100

    Add extra param for non credit cards (#2)
    * Add extra param for non credit cards
    
    * Don't remove expirity fields when non-credit cards are used
    
    * Update version

--- datatrans/gateway/payment_with_alias.py
+++ datatrans/gateway/payment_with_alias.py
@@ -71,6 +71,12 @@ def build_pay_with_alias_request_xml(amount: Money, client_ref: str, alias_regis
     SubElement(request, 'reqtype').text = 'CAA'
     SubElement(request, 'sign').text = sign_mpo(merchant_id, amount, currency, client_ref)
 
+    # For non credit card payment methods who support the creation of an alias
+    # the <pmethod> attribute needs to be submitted
+    # https://api-reference.datatrans.ch/xml/#authorization-with-an-existing-alias
+    if alias_registration.payment_method in ('REK',):
+        SubElement(request, 'pmethod').text = alias_registration.payment_method
+
     return tostring(root, encoding='utf8')
 
 
