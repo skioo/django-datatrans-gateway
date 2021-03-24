0f3c865da5f9b241568ea5acf00fc6e73194067f
Alberto Pou <albertopouquiros@gmail.com>
2021-03-24 13:18:30 +0100

    Django 3 and Python 3.8 support

--- tests/urls.py
+++ tests/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import include, url
+from django.urls import include, path
 
 from datatrans.views import example
 
 urlpatterns = [
-    url(r'^datatrans/', include('datatrans.urls')),
-    url(r'^example/register-credit-card$', example.register_credit_card, name='example_register_credit_card'),
-    url(r'^example/pay$', example.pay, name='example_pay'),
+    path(r'^datatrans/', include('datatrans.urls')),
+    path(r'^example/register-credit-card$', example.register_credit_card, name='example_register_credit_card'),
+    path(r'^example/pay$', example.pay, name='example_pay'),
 ]
